8823bd50366e3df5bfd23b0760524ca917524146
sva273 <spakv577@gmail.com>
2025-09-24 19:33:03 +0200

    Добавил manage.py, requirements.txt, настройки и миграции

--- store_8_09_25/settings.py
+++ store_8_09_25/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products.apps.ProductsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
