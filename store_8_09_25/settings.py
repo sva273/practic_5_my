7675a6f717265bc36b4de9bafa1d7c5771b6d5bd
sva273 <spakv577@gmail.com>
2025-09-25 14:29:07 +0200

    update settings.py, urls.py

--- store_8_09_25/settings.py
+++ store_8_09_25/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
--- store_8_09_25/urls.py
+++ store_8_09_25/urls.py
@@ -15,8 +15,38 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from products.views.categories import CategoryViewSet
+from products.views.suppliers import SupplierViewSet
+from products.views.products import ProductDetail1UpdateDeleteView, ProductListCreateView, ProductViewSet
+from products.views.product_details import ProductDetailUpdateDeleteView, ProductDetailListCreateView
+from products.views.addresses import AddressViewSet
+from products.views.customers import CustomerListCreateView, CustomerDetailsUpdateDeleteView, CustomerViewSet
+from products.views.orders import OrderListCreateView, OrderDetailUpdateDeleteView
+from products.views.orderitems import OrderItemListCreateView, OrderItemDetailUpdateDeleteView
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'suppliers', SupplierViewSet)
+router.register(r'addresses', AddressViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'customers', CustomerViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
+    path('api/v1/products/', ProductListCreateView.as_view()),
+    path('api/v1/products/<int:pk>/', ProductDetail1UpdateDeleteView.as_view()),
+    path('api/v1/product-details/', ProductDetailListCreateView.as_view()),
+    path('api/v1/product-details/<int:pk>/', ProductDetailUpdateDeleteView.as_view()),
+    path('api/v1/customers/', CustomerListCreateView.as_view()),
+    path('api/v1/customer/<int:pk>/', CustomerDetailsUpdateDeleteView.as_view()),
+    path('api/v1/orders/', OrderListCreateView.as_view()),
+    path('api/v1/orders/<int:pk>/', OrderDetailUpdateDeleteView.as_view()),
+    path('api/v1/order-items/', OrderItemListCreateView.as_view()),
+    path('api/v1/order-items/<int:pk>/', OrderItemDetailUpdateDeleteView.as_view()),
+
 ]
